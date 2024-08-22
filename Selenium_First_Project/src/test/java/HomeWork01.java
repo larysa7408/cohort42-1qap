10dc6dc43944ce6ecc19a7fa382cf9a0ee4c4613
larysa7408 <larisa7408tema@gmail.com>
2024-08-22 23:07:14 +0200

    Create HomeWork01.java
    Created a new test class where the site page will open.

---
+++ Selenium_First_Project/src/test/java/HomeWork01.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeWork01{
+    WebDriver driver;
+    @BeforeEach
+    public void setUp(){
+        driver=new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com/");
+    }
+    @Test
+    public void openDemoWebShop(){
+        System.out.println("Demo Web Shop");
+    }
+}
