6962f98c19873798e21621cae6727d4b3ed030c1
Artur Schneider <artur@schneider-dev.ru>
2024-08-28 23:41:20 +0300

    add ChromeTest

---
+++ Selenium_First_Project/src/test/java/lesson/test/ChromeTest.java
@@ -0,0 +1,58 @@
+package lesson.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ChromeTest {
+
+    // Создаем драйвер
+   protected WebDriver driver;
+    // Создаём явные ожидания
+   protected WebDriverWait wait;
+
+
+    @BeforeEach
+// - эта аннотация указание, что метод будет выполняться перед каждым нашим тестовым методом
+//    @BeforeAll // - эта аннотация указание, что метод будет выполняться перед всеми нашими тестовым методом
+    void startDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-search-engine-choice-screen");
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(7));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void closeDriver() {
+        driver.quit();
+    }
+
+    @Test
+    void test2() {
+        driver.navigate().to("https://demowebshop.tricentis.com/");
+        waitInSeconds(10);
+    }
+
+    protected void waitInSeconds(int seconds) {
+        try{
+            Thread.sleep(Duration.ofSeconds(seconds).toMillis());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
